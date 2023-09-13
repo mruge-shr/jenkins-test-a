55514f046163a91c89c6f40ec33e766a34ceb524
Matthew Ruge <Matthew.Ruge@shrgroupllc.com>
2023-09-13 10:38:53 -0400

    try

--- fullpipeline.groovy
+++ fullpipeline.groovy
@@ -1,1 +1,10 @@
-println 'Hello from PipelineGroovy'
+println 'Start Full Pipeline'
+stage('FP-stage') {
+    agent {
+        docker { image 'alpine' }
+    }
+    step {
+        sh 'In a Container!!!'
+    }
+}
+println 'End Full Pipeline'
