7553a84ff3f05f9a55853ddbb11c5720031e1cf8
Matthew Ruge <Matthew.Ruge@shrgroupllc.com>
2023-09-13 10:46:12 -0400

    wrap in step

--- fullpipeline.groovy
+++ fullpipeline.groovy
@@ -1,10 +1,8 @@
 println 'Start Full Pipeline'
-stage('FP-stage') {
+step {
     agent {
         docker { image 'alpine' }
     }
-    step {
-        sh 'In a Container!!!'
-    }
+    sh 'In a Container!!!'
 }
 println 'End Full Pipeline'
